4b9eba63202c94d20f22331781a078ef88409804
kacper-holowaty <kacper.holowaty@gmail.com>
2025-05-10 11:44:56 +0200

    :heavy_check_mark: Create tests for get projects

--- tests/backend-e2e/src/get-projects.spec.ts
+++ tests/backend-e2e/src/get-projects.spec.ts
@@ -1,32 +1,68 @@
-import { APIRequestContext } from '@playwright/test';
 import { test, expect } from './fixtures/project-data';
 import { authenticateUser } from './helpers/auth';
 import { baseUrl } from '../playwright.config';
 
-let token: string;
-let apiContext: APIRequestContext;
+test('should return a list of projects for user with projects', async ({ playwright }) => {
+  const authResponse = await authenticateUser('johndoe@mail.com', '1qazXSW@');
+  const token = authResponse.token;
+  const userData = authResponse.user;
 
-test.beforeAll(async ({ playwright }) => {
-  const authenticationResponse = await authenticateUser('johndoe@mail.com', '1qazXSW@');
-  token = authenticationResponse.token;
-
-  apiContext = await playwright.request.newContext({
+  const apiContext = await playwright.request.newContext({
     baseURL: baseUrl,
     extraHTTPHeaders: {
       Authorization: `Bearer ${token}`,
     },
   });
-});
 
-test.afterAll(async () => {
+  const response = await apiContext.get('/api/v1/projects');
+  
+  expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  
+  expect(responseBody).toBeInstanceOf(Array);
+  // expect(responseBody.length).toBe(2);
+  
+  responseBody.forEach(project => {
+    expect(project).toHaveProperty('id');
+    expect(project).toHaveProperty('name');
+    expect(project).toHaveProperty('description');
+    expect(project).toHaveProperty('status');
+    expect(project).toHaveProperty('createdAt');
+    expect(project).toHaveProperty('updatedAt');
+    expect(project).toHaveProperty('startDate');
+    expect(project).toHaveProperty('endDate');
+    expect(project).toHaveProperty('completedTasks');
+    expect(project).toHaveProperty('totalTasks');
+    expect(project).toHaveProperty('tasks');
+    expect(project).toHaveProperty('members');
+    expect(project).toHaveProperty('owner');
+    
+    expect(project.owner.username).toBe(userData.username);
+    expect(project.owner.firstName).toBe(userData.firstName);
+    expect(project.owner.lastName).toBe(userData.lastName);
+  });
+
   await apiContext.dispose();
 });
 
-test('should return a list of projects', async () => {
-  const response = await apiContext.get('/api/v1/projects');
 
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
+test('should return an empty array for user without projects', async ({ playwright }) => {
+    const authResponse = await authenticateUser('testowy@mail.com', '1qazXSW@');
+    const token = authResponse.token;
 
-  expect(responseBody).toBeInstanceOf(Array);
-});
+    const apiContext = await playwright.request.newContext({
+      baseURL: baseUrl,
+      extraHTTPHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const response = await apiContext.get('/api/v1/projects');
+    
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toBeInstanceOf(Array);
+    expect(responseBody).toEqual([]);
+
+    await apiContext.dispose();
+  });
