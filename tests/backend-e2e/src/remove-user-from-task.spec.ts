0c6c76cf1394eb0ae111d8215237370810cb397b
kacper-holowaty <kacper.holowaty@gmail.com>
2025-05-10 18:28:30 +0200

    :white_check_mark: Add tests add and remove user from task

---
+++ tests/backend-e2e/src/remove-user-from-task.spec.ts
@@ -0,0 +1,102 @@
+import { APIRequestContext } from '@playwright/test';
+import { test, expect } from './fixtures/project-data';
+import { authenticateUser } from './helpers/auth';
+import { baseUrl } from '../playwright.config';
+
+let token: string;
+let apiContext: APIRequestContext;
+
+test.beforeAll(async ({ playwright }) => {
+  const authenticationResponse = await authenticateUser('johndoe@mail.com', '1qazXSW@');
+  token = authenticationResponse.token;
+
+  apiContext = await playwright.request.newContext({
+    baseURL: baseUrl,
+    extraHTTPHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+});
+
+test.afterAll(async () => {
+  await apiContext.dispose();
+});
+
+test('should remove a user from a task', async ({ projectId, taskId }) => {
+  const username = 'john_doe';
+  const userData = {
+    username: username,
+  };
+
+  const response = await apiContext.patch(`/api/v1/projects/${projectId}/tasks/${taskId}/user/remove`, {
+    data: userData,
+  });
+
+  expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.members.some(user => user.username === userData.username)).toBe(false);
+});
+
+test('should not remove user (not a project member) from a task', async ({ projectId, taskId }) => {
+  const username = 'jakis_username';
+  const userData = {
+    username: username,
+  };
+
+  const response = await apiContext.patch(`/api/v1/projects/${projectId}/tasks/${taskId}/user/remove`, {
+    data: userData,
+  });
+
+  expect(response.status()).toBe(401);
+  const responseBody = await response.json();
+
+  expect(responseBody.httpStatus).toBe('UNAUTHORIZED');
+  expect(responseBody.message).toBe(`User is not a member of the task`);
+});
+
+test('should not remove non-existing user from a task', async ({ projectId, taskId }) => {
+  const username = 'non_existing';
+  const userData = {
+    username: username,
+  };
+
+  const response = await apiContext.patch(`/api/v1/projects/${projectId}/tasks/${taskId}/user/remove`, {
+    data: userData,
+  });
+
+  expect(response.status()).toBe(404);
+  const responseBody = await response.json();
+  expect(responseBody.httpStatus).toBe('NOT_FOUND');
+  expect(responseBody.message).toBe(`No user found with username: ${username}`);
+});
+
+test('should return 404 if task is not found', async ({ projectId }) => {
+  const nonExistentTaskId = '00000000-0000-0000-0000-000000000000';
+  const userData = {
+    username: 'john_doe',
+  };
+
+  const response = await apiContext.patch(`/api/v1/projects/${projectId}/tasks/${nonExistentTaskId}/user/remove`, {
+    data: userData,
+  });
+
+  expect(response.status()).toBe(404);
+  const responseBody = await response.json();
+  expect(responseBody.httpStatus).toBe("NOT_FOUND");
+  expect(responseBody.message).toBe(`No task found with id: ${nonExistentTaskId}`);
+});
+
+test('should return error if task has invalid id', async ({ projectId }) => {
+  const invalidTaskId = 'invalid-id';
+  const userData = {
+    username: 'john_doe',
+  };
+
+  const response = await apiContext.patch(`/api/v1/projects/${projectId}/tasks/${invalidTaskId}/user/remove`, {
+    data: userData,
+  });
+
+  expect(response.status()).toBe(400);
+
+  // daje 500 zamiast 400
+});
