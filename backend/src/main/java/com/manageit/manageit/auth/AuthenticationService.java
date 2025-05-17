4dc1560d4d3556f6fbea283367140a65736d1df8
Paweł Zawistowski <p.zawistowski.630@studms.ug.edu.pl>
2025-05-17 14:21:43 +0200

    :art: Refactor(JWT): Update JWT expiration settings; implement refresh token functionality and modify authentication response structure

--- backend/src/main/java/com/manageit/manageit/auth/AuthenticationService.java
+++ backend/src/main/java/com/manageit/manageit/auth/AuthenticationService.java
@@ -1,17 +1,25 @@
 package com.manageit.manageit.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manageit.manageit.configuration.security.JwtService;
+import com.manageit.manageit.core.exception.JwtAuthenticationException;
 import com.manageit.manageit.feature.user.model.User;
 import com.manageit.manageit.feature.user.mapper.UserMapper;
 import com.manageit.manageit.feature.user.repository.UserRepository;
 import com.manageit.manageit.feature.user.dto.AuthenticatedUserResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -31,10 +39,11 @@ public class AuthenticationService {
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .build();
-        repository.save(user);
+        User savedUser = repository.save(user);
         AuthenticatedUserResponseDto authenticatedUserResponseDto = userMapper.toAuthenticatedUserResponse(user);
-        String jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).user(authenticatedUserResponseDto).build();
+        String jwtToken = jwtService.generateToken(savedUser);
+        String refreshToken = jwtService.generateRefreshToken(savedUser);
+        return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).user(authenticatedUserResponseDto).build();
     }
 
     public AuthenticationResponse authenticate(@Valid AuthenticationRequest request) {
@@ -47,7 +56,41 @@ public class AuthenticationService {
         User user = ((User) auth.getPrincipal());
         AuthenticatedUserResponseDto authenticatedUserResponseDto = userMapper.toAuthenticatedUserResponse(user);
         String jwtToken  = jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).user(authenticatedUserResponseDto).build();
+        String refreshToken = jwtService.generateRefreshToken(user);
+        return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).user(authenticatedUserResponseDto).build();
     }
 
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null && request.getQueryString() != null && request.getQueryString().startsWith("token=Bearer")) {
+            authHeader = request.getQueryString().replace("token=Bearer%20", "Bearer ");
+        }
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        final String refreshToken = authHeader.substring(7);
+        final String userId;
+
+        try {
+            userId = jwtService.extractUserId(refreshToken);
+        } catch (Exception e) {
+            throw new JwtAuthenticationException("Invalid JWT token", e);
+        }
+
+        if (userId != null) {
+            User user = repository.findById(UUID.fromString(userId)).orElseThrow(() -> new JwtAuthenticationException("Invalid JWT token"));
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                String accessToken = jwtService.generateToken(user);
+                AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            } else {
+                throw new JwtAuthenticationException("Invalid JWT token");
+            }
+        }
+    }
 }
