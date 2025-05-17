df3a886f784cafbc98c2e7d7d633ca138e9e7458
kacper-holowaty <kacper.holowaty@gmail.com>
2025-05-17 14:03:33 +0200

    :bug: Update validators for creating project

---
+++ backend/src/main/java/com/manageit/manageit/feature/project/validation/EndDateAfterStartDateValidator.java
@@ -0,0 +1,31 @@
+package com.manageit.manageit.feature.project.validation;
+
+import com.manageit.manageit.feature.project.dto.CreateProjectRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EndDateAfterStartDateValidator implements ConstraintValidator<EndDateAfterStartDate, CreateProjectRequest> {
+
+    @Override
+    public void initialize(EndDateAfterStartDate constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(CreateProjectRequest request, ConstraintValidatorContext context) {
+        if (request.getStartDate() == null || request.getEndDate() == null) {
+            return true;
+        }
+
+        boolean isValid = request.getEndDate().isAfter(request.getStartDate());
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("End date must be after start date")
+                    .addPropertyNode("endDate")
+                    .addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
