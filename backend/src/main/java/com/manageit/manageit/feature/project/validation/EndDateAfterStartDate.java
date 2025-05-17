c1214587bf9501690afb762250e6a8716a0a7b14
kacper-holowaty <kacper.holowaty@gmail.com>
2025-05-17 15:50:25 +0200

    :bug: Update validators for updating projects

--- backend/src/main/java/com/manageit/manageit/feature/project/validation/EndDateAfterStartDate.java
+++ backend/src/main/java/com/manageit/manageit/feature/project/validation/EndDateAfterStartDate.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = EndDateAfterStartDateValidator.class)
+@Constraint(validatedBy = {EndDateAfterStartDateForCreateValidator.class, EndDateAfterStartDateForUpdateValidator.class})
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EndDateAfterStartDate {
