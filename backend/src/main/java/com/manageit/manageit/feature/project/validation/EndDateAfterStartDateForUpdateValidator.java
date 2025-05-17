30d2fe6d3f9b39377cc51f61534f52ec75f62099
kacper-holowaty <kacper.holowaty@gmail.com>
2025-05-17 22:12:36 +0200

    :fire: Remove unnecessary comments

--- backend/src/main/java/com/manageit/manageit/feature/project/validation/EndDateAfterStartDateForUpdateValidator.java
+++ backend/src/main/java/com/manageit/manageit/feature/project/validation/EndDateAfterStartDateForUpdateValidator.java
@@ -4,32 +4,6 @@ import com.manageit.manageit.feature.project.dto.UpdateProjectRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-//public class EndDateAfterStartDateForUpdateValidator implements ConstraintValidator<EndDateAfterStartDateForUpdate, UpdateProjectRequest> {
-//
-//    @Override
-//    public void initialize(EndDateAfterStartDateForUpdate constraintAnnotation) {
-//
-//    }
-//
-//    @Override
-//    public boolean isValid(UpdateProjectRequest request, ConstraintValidatorContext context) {
-//        if (request.getStartDate() == null || request.getEndDate() == null) {
-//            return true;
-//        }
-//
-//        boolean isValid = request.getEndDate().isAfter(request.getStartDate());
-//
-//        if (!isValid) {
-//            context.disableDefaultConstraintViolation();
-//            context.buildConstraintViolationWithTemplate("End date must be after start date")
-//                    .addPropertyNode("endDate")
-//                    .addConstraintViolation();
-//        }
-//
-//        return isValid;
-//    }
-//}
-
 public class EndDateAfterStartDateForUpdateValidator implements ConstraintValidator<EndDateAfterStartDate, UpdateProjectRequest> {
 
     @Override
