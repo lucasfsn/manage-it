faa3371f580003ef93974c65fc3ea2e826875e48
Paweł Zawistowski <p.zawistowski.630@studms.ug.edu.pl>
2025-05-24 00:36:22 +0200

    :sparkles: Add response and exception DTOs for authentication endpoints

--- backend/src/main/java/com/manageit/manageit/feature/user/dto/AuthenticatedUserResponseDto.java
+++ backend/src/main/java/com/manageit/manageit/feature/user/dto/AuthenticatedUserResponseDto.java
@@ -3,12 +3,15 @@ package com.manageit.manageit.feature.user.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class AuthenticatedUserResponseDto {
+    private UUID id;
     @JsonProperty("username")
     private String name;
     private String firstName;
