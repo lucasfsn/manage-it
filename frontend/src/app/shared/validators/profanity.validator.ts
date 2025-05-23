c8a37a83b728604778cebfc97e6a04a18f9ff1ec
lucasfsn <luki424221@gmail.com>
2025-05-23 19:42:52 +0200

    :art: Remove profanity words constant

--- frontend/src/app/shared/validators/profanity.validator.ts
+++ frontend/src/app/shared/validators/profanity.validator.ts
@@ -1,4 +1,3 @@
-import { PROFANITY_WORDS } from '@/app/shared/constants/profanity-words';
 import { Profanity } from '@2toad/profanity';
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
@@ -6,7 +5,6 @@ const profanity = new Profanity({
   languages: ['en', 'de'],
   wholeWord: false,
 });
-profanity.addWords(PROFANITY_WORDS);
 
 export function profanityValidator(message: string): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
