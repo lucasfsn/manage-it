cf40b10b9d326d0a935ad26fdb7db9fe0c77aae8
Paweł Zawistowski <147144568+pz090922@users.noreply.github.com>
2025-05-24 14:40:13 +0200

    :art: Update exception response structure (#21)

--- backend/src/main/java/com/manageit/manageit/shared/dto/FieldValidationErrorsDto.java
+++ backend/src/main/java/com/manageit/manageit/shared/dto/FieldValidationErrorsDto.java
@@ -1,17 +1,17 @@
 package com.manageit.manageit.shared.dto;
 
-import com.manageit.manageit.shared.enums.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class FieldValidationErrorsDto {
-    private ErrorCode errorCode;
+    private HttpStatus errorCode;
     private String field;
     private String message;
 }
