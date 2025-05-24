ad01acd222cd11af56c154a9862557a1b09c750f
Paweł Zawistowski <p.zawistowski.630@studms.ug.edu.pl>
2025-05-24 17:08:57 +0200

    :art: Add NoResourceFoundException handler and update error response codes to integers

--- backend/src/main/java/com/manageit/manageit/shared/enums/SuccessCode.java
+++ backend/src/main/java/com/manageit/manageit/shared/enums/SuccessCode.java
@@ -1,12 +1,23 @@
 package com.manageit.manageit.shared.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum SuccessCode {
-    RESOURCE_CREATED,
-    RESPONSE_SUCCESSFUL,
-    RESOURCE_UPDATED,
-    RESOURCE_DELETED,
-    RESOURCE_NOT_FOUND,
-    RESOURCE_RESTORED,
+    RESOURCE_CREATED(201),
+    RESPONSE_SUCCESSFUL(200),
+    RESOURCE_UPDATED(200),
+    RESOURCE_DELETED(200),
+    RESOURCE_NOT_FOUND(404),
+    RESOURCE_RESTORED(200),
+    ;
+
+    private final int code;
+
+    SuccessCode(int code) {
+        this.code = code;
+    }
+
 }
 
 
